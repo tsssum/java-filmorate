bc16e3a5c00f18f64a11767eaf6fbbe8b5f18358
Taya <lessum1@mail.ru>
2026-09-09 08:47:55 +0300

    java-filmorate: wrong annotation used

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,9 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
@@ -13,7 +13,7 @@ public class Film {
     @NotBlank
     String name;
     @NotBlank
-    @Max(200)
+    @Size(max = 200)
     String description;
     @NotNull
     LocalDate releaseDate;
